3bcf8159735195e461cc4ce2a287c2c0a9c207de
julienzhang <zhenmiaoruc@gmail.com>
2021-06-04 18:16:01 +0800

    Delete athena directory

--- athena/athena.sh
+++
@@ -1,5 +0,0 @@
-thread=$1
-bwa index ../metaspades/contigs.fasta
-bwa mem -t $thread -C -p ../metaspades/contigs.fasta ../split_read_parsed_interleaved.fq | samtools sort -@ $thread -o align-reads.metaspades-contigs.bam
-samtools index -@ $thread align-reads.metaspades-contigs.bam
-athena-meta --config config.json
