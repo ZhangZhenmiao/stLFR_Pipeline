69fcb33598c5d1462dbb11f1ae05a4c14c61a261
julienzhang <zhenmiaoruc@gmail.com>
2021-06-04 18:59:05 +0800

    Update test.sh

--- test_run/test.sh
+++ test_run/test.sh
@@ -1,1 +1,1 @@
-bash /path/to/stLFR_Pipe/run_pipe.sh test_stlfr_1.fq.gz test_stlfr_2.fq.gz 60 /path/to/stLFR_Pipe test_output 42
+/path/to/stLFR_Pipe/run_pipe.sh test_stlfr_1.fq.gz test_stlfr_2.fq.gz 60 /path/to/stLFR_Pipe test_output 42
