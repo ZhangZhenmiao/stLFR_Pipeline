c0e7a9e8cc8ec41a43578587f4f4245327b5038d
julienzhang <zhenmiaoruc@gmail.com>
2021-06-04 18:20:57 +0800

    Update test example

--- test_run/test.sh
+++ test_run/test.sh
@@ -1,1 +1,1 @@
-bash ~/stLFR_Pipe/run_pipe.sh test_stlfr_1.fq.gz test_stlfr_2.fq.gz 60 100 ~/stLFR_Pipe test_output
+bash /path/to/stLFR_Pipe/run_pipe.sh test_stlfr_1.fq.gz test_stlfr_2.fq.gz 60 /path/to/stLFR_Pipe test_output 42
